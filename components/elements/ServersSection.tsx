211aceab2ba2341aef925f16f851a8576dff7d9e
baxriddin0317 <baxriddinxojaaxmadaliyev@gmail.com>
2025-08-16 12:52:03 +0500

    add mobile 2 buttons

--- components/elements/ServersSection.tsx
+++ components/elements/ServersSection.tsx
@@ -4,6 +4,7 @@ import { MdAccessTime } from "react-icons/md";
 import ServerItemDropdown from "../server-components/ServerItemDropdown";
 import type { ServerItem } from "@/lib/mockServers";
 import { openedServers, soonServers, todayServers, tomorrowServers } from "@/lib/mockServers";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 function Section({
   title,
@@ -50,21 +51,49 @@ function Section({
 
 export default function ServersSection() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-y-5 gap-x-3.5 ">
-      <Section 
-        icon={<MdAccessTime className="text-brand-primary-3 dark:text-brand-btn" />}
-        title="Скоро откроются"
-        vip={true}
-        servers={soonServers}
-      />
-      <Section
-        icon={<IoRocketSharp className="text-brand-primary-3 dark:text-brand-btn" />}
-        title="Уже открылись"
-        vip={true}
-        servers={openedServers}
-      />
-      <Section title="Сегодня" subtitle="(26.07.2021 - Пятница)" servers={todayServers} />
-      <Section title="Завтра" subtitle="(26.07.2021 - Пятница)" servers={tomorrowServers} />
-    </div>
+    <>
+      <Tabs defaultValue="soon" className="md:hidden w-full">
+        <TabsContent value="soon">
+          <Section 
+            icon={<MdAccessTime className="text-brand-primary-3 dark:text-brand-btn" />}
+            title="Скоро откроются"
+            vip={true}
+            servers={soonServers}
+          />
+        </TabsContent>
+        <TabsContent value="opened">
+          <Section
+            icon={<IoRocketSharp className="text-brand-primary-3 dark:text-brand-btn" />}
+            title="Уже открылись"
+            vip={true}
+            servers={openedServers}
+          />
+        </TabsContent>
+        <TabsList className='bg-white dark:bg-brand-primary-3 h-14 grid grid-cols-2 fixed z-50 bottom-0 left-0 justify-start flex-wrap gap-3 w-full'>
+          <TabsTrigger className='data-[state=active]:bg-brand-gray-2 dark:data-[state=active]:bg-brand-btn-gray-3 data-[state=active]:text-brand-btn h-9 rounded-lg !shadow-none cursor-pointer font-bold dark:text-white' value="soon">Открытие скоро</TabsTrigger>
+          <TabsTrigger className='data-[state=active]:bg-brand-gray-2 dark:data-[state=active]:bg-brand-btn-gray-3 data-[state=active]:text-brand-btn h-9 rounded-lg !shadow-none cursor-pointer font-bold dark:text-white' value="opened">Уже открытые</TabsTrigger>
+        </TabsList>
+      </Tabs>
+      <div className="grid md:hidden grid-cols-1 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-y-5 gap-x-3.5 ">
+        <Section title="Сегодня" subtitle="(26.07.2021 - Пятница)" servers={todayServers} />
+        <Section title="Завтра" subtitle="(26.07.2021 - Пятница)" servers={tomorrowServers} />
+      </div>
+      <div className="hidden md:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-y-5 gap-x-3.5 ">
+        <Section 
+          icon={<MdAccessTime className="text-brand-primary-3 dark:text-brand-btn" />}
+          title="Скоро откроются"
+          vip={true}
+          servers={soonServers}
+        />
+        <Section
+          icon={<IoRocketSharp className="text-brand-primary-3 dark:text-brand-btn" />}
+          title="Уже открылись"
+          vip={true}
+          servers={openedServers}
+        />
+        <Section title="Сегодня" subtitle="(26.07.2021 - Пятница)" servers={todayServers} />
+        <Section title="Завтра" subtitle="(26.07.2021 - Пятница)" servers={tomorrowServers} />
+      </div>
+    </>
   );
 }
